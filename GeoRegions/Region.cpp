5fe75602f713693ae60724b34db2534a6c00d096
McKay-J <mckay.jensen@aggiemail.usu.edu>
2017-03-22 20:35:43 -0600

    Just Pushing

---
+++ GeoRegions/Region.cpp
@@ -0,0 +1,355 @@
+//
+// Created by Stephen Clyde on 3/4/17.
+//
+
+#include "Region.h"
+#include "Utils.h"
+#include "World.h"
+#include "Nation.h"
+#include "State.h"
+#include "County.h"
+#include "City.h"
+
+#include <iomanip>
+#include <iostream>
+
+const std::string regionDelimiter = "^^^";
+const int TAB_SIZE = 4;
+const int defaultSize = 10;
+unsigned int Region::m_nextId = 0;
+
+Region* Region::create(std::istream &in)
+{
+    Region* region = nullptr;
+    std::string line;
+    std::getline(in, line);
+    if (line!="")
+    {
+        region = create(line);
+        if (region!= nullptr)
+            region->loadChildren(in);
+    }
+    return region;
+}
+Region* Region::create(const std::string& data)
+{
+    Region* region = nullptr;
+    std::string regionData;
+    unsigned long commaPos = (int) data.find(",");
+    if (commaPos != std::string::npos)
+    {
+        std::string regionTypeStr = data.substr(0,commaPos);
+        regionData = data.substr(commaPos+1);
+
+        bool isValid;
+        RegionType regionType = (RegionType) convertStringToInt(regionTypeStr, &isValid);
+
+        if (isValid)
+        {
+            region = create(regionType, regionData);
+        }
+
+    }
+
+    return region;
+}
+
+Region* Region::create(RegionType regionType, const std::string& data)
+{
+    Region* region = nullptr;
+    std::string fields[3];
+    if (split(data, ',', fields, 3)) {
+
+        // Create the region based on type
+        switch (regionType) {
+            case WorldType:
+                region = new World();
+                break;
+            case NationType:
+                region = new Nation(fields);
+                break;
+            case StateType:
+                region = new State(fields);
+                break;
+            case CityType:
+                region = new City(fields);
+                break;
+            case CountyType:
+                region = new County(fields);
+                break;
+
+            default:
+                break;
+        }
+
+        // If the region isn't valid, git ride of it
+        if (region != nullptr && !region->getIsValid()) {
+            delete region;
+            region = nullptr;
+        }
+    }
+
+    return region;
+}
+
+std::string Region::regionLabel(RegionType regionType)
+{
+    std::string label = "Unknown";
+    switch (regionType)
+    {
+        case Region::WorldType:
+            label = "World";
+            break;
+        case Region::NationType:
+            label = "Nation";
+            break;
+        case Region::StateType:
+            label = "State";
+            break;
+        case Region::CountyType:
+            label = "County";
+            break;
+        case Region::CityType:
+            label = "City";
+            break;
+        default:
+            break;
+    }
+    return label;
+}
+
+Region::Region() { }
+
+Region::Region(RegionType type, const std::string data[]) :
+        m_id(getNextId()), m_regionType(type), m_isValid(true)
+{
+    m_name = data[0];
+    m_population = convertStringToUnsignedInt(data[1], &m_isValid);
+    if (m_isValid)
+        m_area = convertStringToDouble(data[2], &m_isValid);
+}
+
+Region::~Region()
+{
+    if (m_allocated!=0)
+    {
+        for (int i = 0; i < m_regionCount; i++) {
+            delete m_regions[i];
+        }
+        delete [] m_regions;
+    }
+}
+
+std::string Region::getRegionLabel() const
+{
+    return regionLabel(getType());
+}
+
+unsigned int Region::computeTotalPopulation(unsigned int pop)
+{
+
+    unsigned int population = pop;
+
+    for(int i = 0; i<m_regionCount; ++i)
+    {
+        population += m_regions[i]->computeTotalPopulation(m_regions[i]->m_population);
+    }
+    // TODO: implement computeTotalPopulation, such that the result is m_population + the total population for all sub-regions
+    return population;
+}
+
+
+
+void Region::list(std::ostream& out)
+{
+    out << std::endl;
+    out << getName() << ":" << std::endl;
+
+    for (int i=0; i<m_regionCount; i++)
+    {
+        if (m_regions[i]!= nullptr && m_regions[i]->getIsValid())
+            m_regions[i]->display(out, 0, false);
+    }
+
+    // TODO: implement the loop in the list method
+    // foreach subregion, print out
+    //      id    name
+}
+
+void Region::display(std::ostream& out, unsigned int displayLevel, bool showChild)
+{
+    if (displayLevel>0)
+    {
+        out << std::setw(displayLevel * TAB_SIZE) << " ";
+    }
+
+    unsigned totalPopulation = computeTotalPopulation(m_population);
+    double area = getArea();
+    double density = (double) totalPopulation / area;
+
+    // TODO: compute the totalPopulation using a method
+
+    out << std::setw(6) << getId() << "  "
+        << getName() << ", population="
+        << totalPopulation
+        << ", area=" << area
+        << ", density=" << density << std::endl;
+
+    if (showChild)
+    {
+        // TODO: implement loop in display method
+        for(int i = 0; i<m_regionCount; i++)
+        {
+            m_regions[i]->display(out, displayLevel+ 1, showChild);
+        }
+        // foreach subregion
+        //      display that subregion at displayLevel+1 with the same showChild value
+    }
+}
+
+void Region::save(std::ostream& out)
+{
+    out << getType()
+        << "," << getName()
+        << "," << getPopulation()
+        << "," << getArea()
+        << std::endl;
+
+    // TODO: implement loop in save method to save each sub-region
+    // foreach subregion,
+    //      save that region
+for(int i = 0; i<m_regionCount; i++)
+{
+    if(m_regions[i]!= nullptr && m_regions[i]->getIsValid())
+    {
+        m_regions[i]->save(out);
+    }
+}
+    out << regionDelimiter << std::endl;
+}
+
+void Region::validate()
+{
+    m_isValid = (m_area!=UnknownRegionType && m_name!="" && m_area>=0);
+}
+
+void Region::loadChildren(std::istream& in)
+{
+    std::string line;
+    bool done = false;
+    while (!in.eof() && !done)
+    {
+        std::getline(in, line);
+        if (line==regionDelimiter)
+        {
+            done = true;
+        }
+        else
+        {
+            Region* child = create(line);
+            if (child!= nullptr)
+            {
+
+                // TODO: Add the new sub-region to this region
+                this->addRegion(child);
+                child->loadChildren(in);
+            }
+        }
+    }
+}
+
+unsigned int Region::getNextId()
+{
+    if (m_nextId==UINT32_MAX)
+        m_nextId=1;
+
+    return m_nextId++;
+}
+void Region::addRegion(Region * child)
+{
+    if (child!= nullptr)
+    {
+        if (m_allocated == 0) {
+            m_regions = new Region *[10];
+            m_allocated = 10;
+        }
+
+        if (m_regionCount == m_allocated)
+            grow();
+
+        m_regions[m_regionCount++] = child;
+    }
+}
+
+void Region::grow()
+{
+    m_allocated *= 2;
+    Region** newRegion = new Region*[m_allocated];
+
+    for (int i=0; i<m_allocated; i++)
+    {
+        newRegion[i] = m_regions[i];
+    }
+
+    delete [] m_regions;
+
+    m_regions = newRegion;
+}
+
+Region* Region::getSubRegionByIndex(int index)
+{
+
+    Region* result = nullptr;
+    if (m_regionCount>0 && index>=0 && index < m_regionCount)
+    {
+        result = m_regions[index];
+    }
+    return result;
+}
+
+
+Region* Region::findRegionid(unsigned int id)
+{
+    Region* theRegion = nullptr;
+
+    for (int i = 0; i < m_regionCount; ++i)
+    {
+        if (m_regions[i]->getId() == id)
+        {
+           theRegion = m_regions[i];
+        }
+    }
+    return theRegion;
+}
+
+
+void Region::regionRemover(Region* theRegion)
+{
+    int position = 0;
+
+    if(theRegion == nullptr)
+    {
+        std::cout << "The Region does not exist!" << std::endl;
+    }
+    else
+    {
+        for (int i = 0; i < m_regionCount; ++i)
+        {
+         if(m_regions[i]->getId() == theRegion->getId())
+         {
+             position = i;
+         }
+        }
+        theRegion = nullptr;
+        delete theRegion;
+
+
+        for(int j = position; j < m_regionCount - 1; j++)
+        {
+            m_regions[j] = m_regions[j + 1];
+        }
+        --m_regionCount;
+        std::cout << "Deleted!" << std::endl;
+
+    }
+}
---
+++ GeoRegions/cmake-build-debug/CMakeFiles/GeoRegions.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/McMac/Documents/git/1440/1440/GeoRegions/City.cpp" "/Users/McMac/Documents/git/1440/1440/GeoRegions/cmake-build-debug/CMakeFiles/GeoRegions.dir/City.cpp.o"
+  "/Users/McMac/Documents/git/1440/1440/GeoRegions/County.cpp" "/Users/McMac/Documents/git/1440/1440/GeoRegions/cmake-build-debug/CMakeFiles/GeoRegions.dir/County.cpp.o"
+  "/Users/McMac/Documents/git/1440/1440/GeoRegions/CountyUserInterface.cpp" "/Users/McMac/Documents/git/1440/1440/GeoRegions/cmake-build-debug/CMakeFiles/GeoRegions.dir/CountyUserInterface.cpp.o"
+  "/Users/McMac/Documents/git/1440/1440/GeoRegions/Menu.cpp" "/Users/McMac/Documents/git/1440/1440/GeoRegions/cmake-build-debug/CMakeFiles/GeoRegions.dir/Menu.cpp.o"
+  "/Users/McMac/Documents/git/1440/1440/GeoRegions/MenuOption.cpp" "/Users/McMac/Documents/git/1440/1440/GeoRegions/cmake-build-debug/CMakeFiles/GeoRegions.dir/MenuOption.cpp.o"
+  "/Users/McMac/Documents/git/1440/1440/GeoRegions/Nation.cpp" "/Users/McMac/Documents/git/1440/1440/GeoRegions/cmake-build-debug/CMakeFiles/GeoRegions.dir/Nation.cpp.o"
+  "/Users/McMac/Documents/git/1440/1440/GeoRegions/NationUserInterface.cpp" "/Users/McMac/Documents/git/1440/1440/GeoRegions/cmake-build-debug/CMakeFiles/GeoRegions.dir/NationUserInterface.cpp.o"
+  "/Users/McMac/Documents/git/1440/1440/GeoRegions/Region.cpp" "/Users/McMac/Documents/git/1440/1440/GeoRegions/cmake-build-debug/CMakeFiles/GeoRegions.dir/Region.cpp.o"
+  "/Users/McMac/Documents/git/1440/1440/GeoRegions/State.cpp" "/Users/McMac/Documents/git/1440/1440/GeoRegions/cmake-build-debug/CMakeFiles/GeoRegions.dir/State.cpp.o"
+  "/Users/McMac/Documents/git/1440/1440/GeoRegions/StateUserInterface.cpp" "/Users/McMac/Documents/git/1440/1440/GeoRegions/cmake-build-debug/CMakeFiles/GeoRegions.dir/StateUserInterface.cpp.o"
+  "/Users/McMac/Documents/git/1440/1440/GeoRegions/UserInterface.cpp" "/Users/McMac/Documents/git/1440/1440/GeoRegions/cmake-build-debug/CMakeFiles/GeoRegions.dir/UserInterface.cpp.o"
+  "/Users/McMac/Documents/git/1440/1440/GeoRegions/Utils.cpp" "/Users/McMac/Documents/git/1440/1440/GeoRegions/cmake-build-debug/CMakeFiles/GeoRegions.dir/Utils.cpp.o"
+  "/Users/McMac/Documents/git/1440/1440/GeoRegions/World.cpp" "/Users/McMac/Documents/git/1440/1440/GeoRegions/cmake-build-debug/CMakeFiles/GeoRegions.dir/World.cpp.o"
+  "/Users/McMac/Documents/git/1440/1440/GeoRegions/WorldUserInterface.cpp" "/Users/McMac/Documents/git/1440/1440/GeoRegions/cmake-build-debug/CMakeFiles/GeoRegions.dir/WorldUserInterface.cpp.o"
+  "/Users/McMac/Documents/git/1440/1440/GeoRegions/main.cpp" "/Users/McMac/Documents/git/1440/1440/GeoRegions/cmake-build-debug/CMakeFiles/GeoRegions.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
